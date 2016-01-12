a99ea37241090624ee7b3b0dc4d3f38da82bcad9
unknown <rhsharma@697-334-Forland.sf.k12.internal>
2016-01-12 10:32:18 -0800

    I added all the code

--- build-tmp/source/GoogleBillboard.java
+++ build-tmp/source/GoogleBillboard.java
@@ -1,3 +1,19 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class GoogleBillboard extends PApplet {
+
 public final static String e = "2.718281828459045235360287471352662497757247093699959574966967627724076630353547594571382178525166427427466391932003059";  
 public void setup()  
 {            
@@ -5,7 +21,7 @@ public void setup()
      String digits = e.substring(0, 10);
      double dnum = Double.parseDouble(digits);
      System.out.println(dnum);
-	for (int i = 0; i<e.length(); i++) {
+	for (int i = 0; i<e.length()+10; i++) {
 		digits = e.substring(i, i+10);
 		if (isPrime(dnum) == true)
 			System.out.println(digits);
@@ -28,3 +44,12 @@ public boolean isPrime(double dNum)
 	}
     return true;  
 } 
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "GoogleBillboard" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
