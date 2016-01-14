f9959571f7746b92fea68d3db72ea82d853f813f
unknown <rhsharma@697-334-Forland.sf.k12.internal>
2016-01-14 10:19:30 -0800

    Made it so that it prints out the correct prime number.

--- build-tmp/source/GoogleBillboard.java
+++ build-tmp/source/GoogleBillboard.java
@@ -14,17 +14,18 @@ import java.io.IOException;
 
 public class GoogleBillboard extends PApplet {
 
-public final static String e = "2.718281828459045235360287471352662497757247093699959574966967627724076630353547594571382178525166427427466391932003059";  
+public final static String e = "2718281828459045235360287471352662497757247093699959574966967627724076630353547594571382178525166427427466391932003059";  
 public void setup()  
 {            
      noLoop();  
      String digits = e.substring(0, 10);
-     double dnum = Double.parseDouble(digits);
-     System.out.println(dnum);
-	for (int i = 0; i<e.length()+10; i++) {
+	for (int i = 0; i<e.length(); i++) {
 		digits = e.substring(i, i+10);
-		if (isPrime(dnum) == true)
-			System.out.println(digits);
+		double dnum = Double.parseDouble(digits);
+		if (isPrime(dnum) == true) {
+     		System.out.println(dnum);
+			break;
+		}
 	}
 }  
 public void draw()  
@@ -32,14 +33,14 @@ public void draw()
 }  
 public boolean isPrime(double dNum)  
 {   
-	if (dNum <= 0)
-		return true;
+	if (dNum == 0)
+		return false;
 	else if (dNum == 1)
-		return true;
+		return false;
 	else if (dNum == 2)
 		return true;
-	for (int i = 0; i <= Math.sqrt(dNum); i++) {
-		if (dNum%2 == 0)
+	for (int i = 2; i <= Math.sqrt(dNum); i++) {
+		if (dNum%i == 0)
 			return false;
 	}
     return true;  
